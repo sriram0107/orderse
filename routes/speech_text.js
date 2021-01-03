c9b45f877083457950e67dd0f130f7836e66cbb6
Sriram Sathish <63444773+sriram0107@users.noreply.github.com>
2021-01-03 19:08:56 +0400

    got blob but not the buffer

--- routes/speech_text.js
+++ routes/speech_text.js
@@ -3,15 +3,16 @@ const fs = require("fs");
 const { IamAuthenticator } = require("ibm-watson/auth");
 const SpeechToTextV1 = require("ibm-watson/speech-to-text/v1");
 const { watson_speech_to_text_config } = require("../watson");
+const fetch = require("node-fetch");
+var toBuffer = require("blob-to-buffer");
 
-router.post("/", (req, res) => {
+router.post("/", async function (req, res) {
   const speechToText = new SpeechToTextV1({
     authenticator: new IamAuthenticator({
       apikey: watson_speech_to_text_config.apikey,
     }),
     serviceUrl: watson_speech_to_text_config.url,
   });
-  console.log(req.body);
 
   const params = {
     objectMode: true,
@@ -19,7 +20,15 @@ router.post("/", (req, res) => {
     model: "en-US_BroadbandModel",
   };
   const recognizeStream = speechToText.recognizeUsingWebSocket(params);
-  fs.createReadStream(req.body.url).pipe(recognizeStream);
+  let file = await fetch(req.body.data)
+    .then((r) => r.blob())
+    .catch((err) => console.log("api :", err));
+  console.log("file -", file);
+  toBuffer(file, function (err, buffer) {
+    if (err) throw err;
+    fs.createReadStream(blob).pipe(recognizeStream);
+  });
+
   recognizeStream.on("data", function (event) {
     onEvent("Data:", event);
   });
