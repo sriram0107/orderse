b766de161cad4cf55590d6bfad880a955fac2ba7
Sriram Sathish <63444773+sriram0107@users.noreply.github.com>
2021-01-05 19:38:23 +0400

    cleaned up the codebase

--- routes/speech_text.js
+++ routes/speech_text.js
@@ -1,51 +1,50 @@
 const router = require("express").Router();
 const fs = require("fs");
+const fsp = require("fs-promise");
 const { IamAuthenticator } = require("ibm-watson/auth");
 const SpeechToTextV1 = require("ibm-watson/speech-to-text/v1");
 const { watson_speech_to_text_config } = require("../watson");
-var multer = require("multer");
-var upload = multer({ dest: "uploads/" });
+
+const speechToText = new SpeechToTextV1({
+  authenticator: new IamAuthenticator({
+    apikey: watson_speech_to_text_config.apikey,
+  }),
+  serviceUrl: watson_speech_to_text_config.url,
+});
 
 router.post("/", async function (req, res) {
-  const speechToText = new SpeechToTextV1({
-    authenticator: new IamAuthenticator({
-      apikey: watson_speech_to_text_config.apikey,
-    }),
-    serviceUrl: watson_speech_to_text_config.url,
-  });
+  const buffer = req.files[0].buffer; //Extract audio file as a hexadecimal buffer
+  const translationModel = "en-US_BroadbandModel";
   const params = {
     objectMode: true,
     contentType: "audio/wav",
-    model: "en-US_BroadbandModel",
+    model: translationModel,
   };
-  fs.open("./uploads/audio.wav", "w", function (err, fd) {
-    if (err) {
-      throw "could not open file: " + err;
-    }
-    var buffer = req.files[0].buffer;
-    // write the contents of the buffer, from position 0 to the end, to the file descriptor returned in opening our file
-    fs.write(fd, buffer, 0, buffer.length, null, function (err) {
-      if (err) throw "error writing file: " + err;
-      fs.close(fd, function () {
-        console.log("wrote the file successfully");
-        const recognizeStream = speechToText.recognizeUsingWebSocket(params);
-        fs.createReadStream("./uploads/audio.wav").pipe(recognizeStream);
-        recognizeStream.on("data", function (event) {
-          onEvent("Data:", event);
-        });
-        recognizeStream.on("error", function (event) {
-          onEvent("Error:", event);
-        });
-        recognizeStream.on("close", function (event) {
-          onEvent("Close:", event);
-        });
-        function onEvent(name, event) {
-          console.log(name, JSON.stringify(event, null, 2));
-          res.send(name);
-        }
-      });
-    });
+  var textData = null;
+  const recognizeStream = speechToText.recognizeUsingWebSocket(params);
+  recognizeStream.on("data", (event) => {
+    onEvent("Data:", event);
   });
+  recognizeStream.on("error", (event) => {
+    onEvent("Error:", event);
+  });
+  recognizeStream.on("close", (event) => {
+    onEvent("Close:", event);
+  });
+  const onEvent = (name, event) => {
+    textData = event.results ? event.results[0].alternatives[0].transcript : "";
+    console.log(textData);
+  };
+
+  try {
+    const file = await fsp.open("./uploads/audio.wav", "w");
+    await fsp.write(file, buffer, 0, buffer.length, null);
+    await fsp.close(file);
+    await fsp.createReadStream("./uploads/audio.wav").pipe(recognizeStream);
+  } catch (err) {
+    console.log("Error with reading the audio file", err);
+  }
+  res.send(textData);
 });
 
 module.exports = router;
